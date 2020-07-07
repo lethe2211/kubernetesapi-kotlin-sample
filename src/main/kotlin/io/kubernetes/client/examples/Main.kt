ef37cc2eb8b1bf1d39faa54774386c74bec9ce9f
lethe2211 <lethe2211@gmail.com>
2020-07-07 09:15:45 +0900

    Add a sample to list all the Pods

--- src/main/kotlin/io/kubernetes/client/examples/Main.kt
+++ src/main/kotlin/io/kubernetes/client/examples/Main.kt
@@ -1,5 +1,34 @@
 package io.kubernetes.client.examples
 
+import io.kubernetes.client.openapi.ApiClient
+import io.kubernetes.client.openapi.Configuration
+import io.kubernetes.client.openapi.apis.CoreV1Api
+import io.kubernetes.client.openapi.models.V1PodList
+import io.kubernetes.client.util.ClientBuilder
+import io.kubernetes.client.util.Config
+import io.kubernetes.client.util.KubeConfig
+import java.io.FileReader
+
 fun main() {
-    println("Hello")
+    println("Start")
+
+    // file path to your KubeConfig
+    // val kubeConfigPath = "/Users/shuhei.shogen/.kube/config"
+
+    // loading the out-of-cluster config, a kubeconfig from file-system
+    // val client = ClientBuilder.kubeconfig(KubeConfig.loadKubeConfig(FileReader(kubeConfigPath))).build()
+    val client = ClientBuilder.standard().build()
+
+    // set the global default api-client to the in-cluster one from above
+    Configuration.setDefaultApiClient(client)
+
+    // the CoreV1Api loads default api-client from global configuration.
+    val api = CoreV1Api()
+
+    // invokes the CoreV1Api client
+    val list: V1PodList = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null)
+    println("Listing all pods: ")
+    for (item in list.getItems()) {
+        println(item.metadata?.name)
+    }
 }
